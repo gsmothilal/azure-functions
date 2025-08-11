fdba5c19cc3906660f92ee4773ff9c15a3431b7c
gsmothilal <mothilal488@gmail.com>
2025-08-11 15:19:40 +0530

    Update backend.tf

--- terraform/modules/functions/backend.tf
+++ terraform/modules/functions/backend.tf
@@ -1,10 +1,11 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "tfstate-rg"      
-    storage_account_name = "tfstatestorageacct11"      
+    storage_account_name = "mothistorage123"      
     container_name       = "tfstate"                 
     key                  = "functions.tfstate"  
   }
 }
+
 
 
